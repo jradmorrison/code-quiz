db2882a6f38e9effef0845204b11583226b8f92d
jradmorrison <142756649+jradmorrison@users.noreply.github.com>
2023-09-29 16:15:10 -0400

    created the list elements that the questions will be written to and styled. Also wrote the five question that will be asked

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+const jsQuestions = [
+    {
+      title: "What does 'DOM' stand for in JavaScript?",
+      choices: [
+        "Document Object Model", 
+        "Data Object Model", 
+        "Dynamic Object Method",
+        "Document Oriented Markup"
+    ], 
+      answer: "Document Object Model",
+    },
+    {
+      title: "Which of the following is not a JavaScript data type?",
+      choices: [
+        "String",
+        "Boolean",
+        "Float",
+        "Undefined",
+      ],
+      answer: "Float",
+    },
+    {
+      title: "What is the purpose of the 'typeof' operator in JavaScript?",
+      choices: [
+        "To check if a variable is defined",
+        "To convert a value to a string",
+        "To determine the type of a value",
+        "To create a new variable",
+    ],
+      answer: "To determine the type of a value",
+    },
+    {
+      title: "Which keyword is used to declare a variable in JavaScript?",
+      choices: [
+        "let",
+        "variable",
+        "var",
+        "const",
+      ],
+      answer: "let",
+    },
+    {
+      title: "What does 'NaN' stand for in JavaScript?",
+      choices: [
+        "Not a Null",
+        "No Argument Needed",
+        "Not a Number",
+        "No Assignment",
+      ],
+      answer: "Not a Number",
+    },
+  ];
+  
+  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,81 @@
-// ============= Defining elements as variables ===========================
+/*
+
+== variables to keep track of game state ==
+
+current question
+time remaining
+
+
+submit button dom element
+timer element
+answers to choose from
+score - can just be tied in with timer
+
+
+function to pull question from questions array
+assign all answers to a button by looping over the choice array
+compare choice selected against answer
+
+function for click event
+
+if statement it matches the answer, then you move on to the next question, else decrement time the timer
+
+save highscore to local storage
+
+call highscores from a button on homepage
+sort high scores to decending order
+
+*/
+
+// =========================== Defining elements as variables ==============================
 var headEl = document.getElementById('header');
 var highScoresEl = document.getElementById('high-scores');
 var timerEl = document.getElementById('timer');
 var h1El = document.getElementById('h1');
 var h2El = document.getElementById('h2');
 var btnEl = document.getElementById('start');
+var olEl = document.querySelector('.choices')
+var choiceA = document.getElementById('a');
+var choiceB = document.getElementById('b');
+var choiceC = document.getElementById('c');
+var choiceD = document.getElementById('d');
 
-var title = `Coding Quiz Challenge`;
+// ================================= Other variables =========================================
+var heading = `Coding Quiz Challenge`;
 h2El.innerHTML = `Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!`;
 
-var question = [
-    {question: '1', answer: '1', allAnswers: '1,2,3,4'},
-    {question: '2', answer: '2', allAnswers: '1,2,3,4'},
-    {question: '3', answer: '3', allAnswers: '1,2,3,4'},
-    {question: '4', answer: '4', allAnswers: '1,2,3,4'},
-    {question: '5', answer: '5', allAnswers: '1,2,3,4'},
-]
 
+var allQuestions = jsQuestions;
 var questionIndex = 0;
 var secondsLeft = 60;
 
-headEl.setAttribute('style', 'padding: 20px; font-size: 24px; display: flex; width: 100vw; justify-content: space-between; position:absolute; top:0;');
-h1El.setAttribute('style', 'text-align:center;');
-h2El.setAttribute('style', 'text-align:center; font-size:200%;');
-btnEl.setAttribute('style', 'width: 150px; font-size:125%; color:white; background:#351C75; border: 0; padding: 10px 0; border-radius:5px; margin:30px auto;');
+// ===================================== Element styling ==================================
 
-h1El.innerHTML = title;
+// ====================================== Functions ======================================
 
+// =============== Function for Landing page ======================
+function init() {
+
+  h1El.innerHTML = heading;
+  h2El.innerHTML = `Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!`;
+  btnEl.addEventListener('click', startQuiz)
+}
+
+// ================ Function for starting the quiz =================
 function startQuiz() {
     console.log(`Quiz Started`);
     startTimer();
+    h1El.innerHTML = 'question 1'
+    h2El.innerHTML = '';
+    highScoresEl.setAttribute('style', 'display:none;')
+    btnEl.setAttribute('style', 'display:none;')
+    olEl.setAttribute('style', 'display:block;')
+
 }
 
-
-
+// =================== Function for timer ==============================
 function startTimer() {
-    // Sets interval in variable
+
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timerEl.innerHTML = `Time Left: ${secondsLeft}`;
@@ -47,11 +87,13 @@ function startTimer() {
       }
   
     }, 1000);
-  }
-  
-  // Function to create and append colorsplosion image
-  function sendMessage() {
-      console.log(`Times Up!`)
-  }
+}
 
-btnEl.addEventListener('click', startQuiz())
+// ============= Function to send message that time is up ======================
+function sendMessage() {
+      console.log(`Times Up!`)
+      h1El.innerHTML = `Times Up!`
+}
+
+
+init();
